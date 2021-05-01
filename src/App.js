71aba9b29435ccf933e4533feab0a234004f02ed
TIMESX <83513858+TIMESXtoken@users.noreply.github.com>
2021-05-01 20:21:04 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>?</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
